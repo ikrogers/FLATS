502a3f72d0460c24981556e7719042dd5e721050
ikrogers <ikrogers92@gmail.com>
2014-11-06 01:01:26 +0000

    Minor bugfixes and spelling mistakes

--- db/migrate/20141029194030_create_exercise_managers.rb
+++ db/migrate/20141029194030_create_exercise_managers.rb
@@ -1,7 +1,7 @@
 class CreateExerciseManagers < ActiveRecord::Migration
   def change
     create_table :exercise_managers do |t|
-      t.intteger :user_id
+      t.integer :user_id
       t.integer :ex_id
 
       t.timestamps
