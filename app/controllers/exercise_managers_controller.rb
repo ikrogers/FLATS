2c8cf7a00c679a76feabbe6a914b1b942d8d7c9a
Ilya Rogers <ikrogers92@gmail.com>
2014-11-09 15:30:28 -0500

    Added ex manager creating where user can add workouts.

--- app/controllers/exercise_managers_controller.rb
+++ app/controllers/exercise_managers_controller.rb
@@ -1,7 +1,36 @@
 class ExerciseManagersController < ApplicationController
   respond_to :html, :xml, :json
   before_action :set_exercise_manager, only: [:show, :edit, :update, :destroy]
+  def index
+    @exercise_managers = ExerciseManager.where(user_id: current_user.id)
+  end
+  
+  def new
+    @exercise_manager = ExerciseManager.new
+  end
+  
+  def create
+    @man = Exercise.find(params[:project][:sel_ex]) rescue nil
+    @man.each do |e|
+      @em = ExerciseManager.create(user_id: current_user.id, exercise_id: e.id) 
+    end
 
+    respond_to do |format|
+        format.html { redirect_to exercise_managers_path }
+        format.json { render action: 'show', status: :created, location: @diet }
+    end
+  end
+  
+  
+
+    def destroy
+    @em = ExerciseManager.find_by_id(params[:id])
+    @em.destroy
+    respond_to do |format|
+      format.html { redirect_to exercise_managers_url }
+      format.json { head :no_content }
+    end
+  end
   
   private
     def set_exercise_manager
