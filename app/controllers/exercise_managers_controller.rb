0ea591fabbb44cfa810c87b423d8396a854c87d3
ikrogers <ikrogers92@gmail.com>
2014-11-26 15:42:16 -0500

    h

--- app/controllers/exercise_managers_controller.rb
+++ app/controllers/exercise_managers_controller.rb
@@ -4,26 +4,24 @@ class ExerciseManagersController < ApplicationController
   def index
     @exercise_managers = ExerciseManager.where(user_id: current_user.id)
   end
-  
+
   def new
     @exercise_manager = ExerciseManager.new
   end
-  
+
   def create
     @man = Exercise.find(params[:project][:sel_ex]) rescue nil
     @man.each do |e|
-      @em = ExerciseManager.create(user_id: current_user.id, exercise_id: e.id) 
+      @em = ExerciseManager.create(user_id: current_user.id, exercise_id: e.id)
     end
 
     respond_to do |format|
-        format.html { redirect_to exercise_managers_path }
-        format.json { render action: 'show', status: :created, location: @diet }
+      format.html { redirect_to exercise_managers_path }
+      format.json { render action: 'show', status: :created, location: @diet }
     end
   end
-  
-  
 
-    def destroy
+  def destroy
     @em = ExerciseManager.find_by_id(params[:id])
     @em.destroy
     respond_to do |format|
@@ -31,13 +29,14 @@ class ExerciseManagersController < ApplicationController
       format.json { head :no_content }
     end
   end
-  
-  private
-    def set_exercise_manager
-      @exercise_manager = ExerciseManager.find(params[:id])
-    end
 
-    def exercise_manager_params
-      params.require(:exercise_manager).permit(:user_id, :exercise_id)
-    end
+  private
+
+  def set_exercise_manager
+    @exercise_manager = ExerciseManager.find(params[:id])
+  end
+
+  def exercise_manager_params
+    params.require(:exercise_manager).permit(:user_id, :exercise_id)
+  end
 end
