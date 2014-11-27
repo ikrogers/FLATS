84eec7453fb9e77aa93ee877e861b1fb9b8bed88
Ilya Rogers <ikrogers92@gmail.com>
2014-11-26 22:59:43 -0500

    Added finess test and modded registration

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -13,21 +13,28 @@ class HomepageController < ApplicationController
     @height = params[:height] rescue nil
     @weight = params[:weight] rescue nil
     @birthday = params[:birth] rescue nil
+    @first = params[:first] rescue nil
+    @first = params[:last] rescue nil
+
     if @gender != nil && @height != nil
       if @weight != nil && @birthday != nil
-        @user.update_attributes(:gender => @gender, :height=> @height, :weight => @weight, :bday => @birthday)
+        if @first != nil && @last != nil
+        @user.update_attributes(:gender => @gender, :height=> @height, :weight => @weight, :bday => @birthday, :fname => @first, :lname => @last)
+        end
       end
     end
-    
-        respond_to do |format|
-          if @gender != nil && @height != nil
-      if @weight != nil && @birthday != nil
+
+    respond_to do |format|
+      if @gender != nil && @height != nil
+        if @weight != nil && @birthday != nil
+          if @first != nil && @last != nil
           format.html{redirect_to authenticated_root_path, notice: 'Thank you! Enjoy!'}
+          end
         end
       end
       format.html{redirect_to authenticated_root_path, alert: 'An error occured... probably some of the fiels are not filled out correctly'}
     end
-          
-     end
+
   end
+end
 
