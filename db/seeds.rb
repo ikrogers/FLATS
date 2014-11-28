b6d0eca94cc3056a892e7288a5b7e13225ba8946
jkb6292 <jkb6292@gmail.com>
2014-11-28 13:44:17 -0500

    Updated database table for design

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,16 +10,16 @@ User.create(:email => "ik@valdosta.edu", :fname => "Ilya", :lname => "Rogers", :
 User.create(:email => "ik2@valdosta.edu", :fname => "Ilya", :lname => "Rogers", :password => "123123123", :password_confirmation => "123123123", :u_type => 3)
 User.create(:email => "tccauley@valdosta.edu", :fname => "Clinton", :lname => "Cauley", :password => "789789789", :password_confirmation => "789789789", :u_type => 2)
 
+#Exercises are labeled 1-6 in intensity. I split them together into their different groups based on calories lost more than anything ele, just to be simple
+
+#Exercises with intensity 1 - >= 100 calories lost and relatively easy
 Exercise.create(
-    :name => "Behind Head Chest Stretch", 
-    :descr => "1.) In this stretch you will need a partner behind you to assist.
-        
-        2.) Start off by sitting upright on the floor with your partner behind you, placing your hands behind your head and legs extended out in front of you with a slight bend in the kees.
-        
-        3.) Have your partner hold your elbows, then try and pull your elbows forward from behind your head for about 15 to 30 seconds then release.
-        
-        4.) Then afterwards have your partner pull back on your elbows again but easily this time to help stretch out the chest muscles.", 
-    :calories => 180, 
+    :name => "Cat Stretch", 
+    :descr => "1.) Start off kneeling down on the floor with your feet extended behind you and your hands flat on the floor at shoulder level.
+        2.) Slowly pull your belly in and round out your back, letting your head drop to the floor until you feel a stretch in your back muscles.
+        3.) Hold this position for 15 to 30 seconds then return back to the starting position.
+        4.) Repeat for as long and many reps as desired.", 
+    :calories => 100, 
     :intensity => 1)
 
 Exercise.create(
@@ -40,28 +40,101 @@ Exercise.create(
         Running improves on conditioning the body, endurance, stamina, speed, strength in the legs and core, as well as improving one's posture.", 
     :calories => 90, 
     :intensity => 1)
+    
+Exercise.create(
+    :name => "Tai Chi(60 minutes)",
+    :descr => "Tai chi is a very slow paced, relaxing and gentle exercise routine based on breathing and stretching.",
+    :calories => 272,
+    :intensity => 1)
+    
+Exercise.create(
+    :name => "Chair Dancing",
+    :descr => "Chair dancing is a simple, seated exercise program that allows you to burn calories in a fun and relaxing way.",
+    :calories => 150,
+    :intensity => 1)
 
+#Exercises with intensity 2 - More difficult than 1 but not by much. Also low calories burned
 Exercise.create(
     :name => "Running (3mph/30minutes)", 
     :descr => "Running is one of the most common and effect forms of exercise for muscle toning, weight loss and keeping your lungs and body in shape.
         Running improves on conditioning the body, endurance, stamina, speed, strength in the legs and core, as well as improving one's posture.", 
     :calories => 120, 
-    :intensity => 1)
+    :intensity => 2)
 
+Exercise.create(
+    :name => "Behind Head Chest Stretch", 
+    :descr => "1.) In this stretch you will need a partner behind you to assist.
+        
+        2.) Start off by sitting upright on the floor with your partner behind you, placing your hands behind your head and legs extended out in front of you with a slight bend in the kees.
+        
+        3.) Have your partner hold your elbows, then try and pull your elbows forward from behind your head for about 15 to 30 seconds then release.
+        
+        4.) Then afterwards have your partner pull back on your elbows again but easily this time to help stretch out the chest muscles.", 
+    :calories => 180, 
+    :intensity => 2)
+
+Exercise.create(
+    :name => "Pilates", 
+    :descr => "Pilates are an exercise that focus upon developing strength through the core of the body as well as increasing flexibility and coordination.", 
+    :calories => 180, 
+    :intensity => 2)
+
+Exercise.create(
+    :name => "Abdominal Pendulum", 
+    :descr => "1.) Start off by laying with your back flat on the floor, your legs together extended out in front of you and your hands placed out to your sides so that you are in a 'T' position.
+        
+        2.) Slowly lift your legs up and over your body so that you are creating a right angle to the floor, then twisting only at your hips, lower your feet slowly to the floor to your right without losing any control.
+        
+        3.) As soon as you feel a stretch in your obliques, return to the starting position and repeat on the opposite side.
+        
+        4.) Repeat for as many reps and sets as desired.", 
+    :calories => 180, 
+    :intensity => 2)
+    
+Exercise.create(
+    :name => "Jumping Rope", 
+    :descr => "Jump rope is a great cardio exercise that helps burn a significant amount of calories through repeated jumping motion and keeping the body moving.", 
+    :calories => 200, 
+    :intensity => 2)
+
+#Exercises with intensity 3    
+Exercise.create(
+    :name => "Indoor Cycling", 
+    :descr => "Indoor cycling is a form of exercise that focuses on endurance, stamina, strength, intensity, intervals and recovery.", 
+    :calories => 200, 
+    :intensity => 3)
+    
+Exercise.create(
+    :name => "Aerobic", 
+    :descr => "Aerobic Exercise focuses upon cardiovascular fitness, muscle strength and endurance by performing aerobic workouts.", 
+    :calories => 230, 
+    :intensity => 3)
+    
+Exercise.create(
+    :name => "Swimming (20 minutes)",
+    :descr => "Swimming laps is a fun and excellent way to work out your entire body, while burning fat in the process!",
+    :calories => 106,
+    :intensity => 3)
+    
+Exercise.create(
+    :name => "Running (4mph, 30 minutes)",
+    :descr => "Running is one of the most common and effect forms of exercise for muscle toning, weight loss and keeping your lungs and body in shape.
+        Running improves on conditioning the body, endurance, stamina, speed, strength in the legs and core, as well as improving one's posture.", 
+    :calories => 265,
+    :intensity => 3)
+    
+Exercise.create(
+    :name => "Volleyball (30 minutes)",
+    :descr => "A fun and engaging sport that gets you running around and moving your body with friends, burning calories along the way",
+    :calories => 206,
+    :intensity => 3)
+
+#Exercises with intensity 4
 Exercise.create(
     :name => "Aerobics (moderate/30minutes)", 
     :descr => "Aerobic Exercise focuses upon cardiovascular fitness, muscle strength and endurance by performing aerobic workouts.", 
     :calories => 232, 
-    :intensity => 2)
-    
-Exercise.create(
-    :name => "Cat Stretch", 
-    :descr => "1.) Start off kneeling down on the floor with your feet extended behind you and your hands flat on the floor at shoulder level.
-        2.) Slowly pull your belly in and round out your back, letting your head drop to the floor until you feel a stretch in your back muscles.
-        3.) Hold this position for 15 to 30 seconds then return back to the starting position.
-        4.) Repeat for as long and many reps as desired.", 
-    :calories => 100, 
-    :intensity => 1)
+    :intensity => 4)
     
 Exercise.create(
     :name => "Bench Dip", 
@@ -75,51 +148,28 @@ Exercise.create(
         
         5.) Repeat for as many reps and sets as desired.", 
     :calories => 290, 
-    :intensity => 2)
-    
-Exercise.create(
-    :name => "Abdominal Pendulum", 
-    :descr => "1.) Start off by laying with your back flat on the floor, your legs together extended out in front of you and your hands placed out to your sides so that you are in a 'T' position.
-        
-        2.) Slowly lift your legs up and over your body so that you are creating a right angle to the floor, then twisting only at your hips, lower your feet slowly to the floor to your right without losing any control.
-        
-        3.) As soon as you feel a stretch in your obliques, return to the starting position and repeat on the opposite side.
-        
-        4.) Repeat for as many reps and sets as desired.", 
-    :calories => 180, 
-    :intensity => 1)
-    
-Exercise.create(
-    :name => "Aerobic", 
-    :descr => "Aerobic Exercise focuses upon cardiovascular fitness, muscle strength and endurance by performing aerobic workouts.", 
-    :calories => 230, 
-    :intensity => 1)
-    
-Exercise.create(
-    :name => "Pilates", 
-    :descr => "Pilates are an exercise that focus upon developing strength through the core of the body as well as increasing flexibility and coordination.", 
-    :calories => 180, 
-    :intensity => 1)
+    :intensity => 4)
     
 Exercise.create(
     :name => "Mountain Biking", 
     :descr => "Mountain biking is a sport that consists of riding outdoors, mostly off-road courses and over rough terrain.
         It requires stamina, core strength, endurance, balance, coordination and handling skills.", 
     :calories => 240, 
-    :intensity => 1)
+    :intensity => 4)
     
 Exercise.create(
-    :name => "Jumping Rope", 
-    :descr => "Jump rope is a great cardio exercise that helps burn a significant amount of calories through repeated jumping motion and keeping the body moving.", 
-    :calories => 200, 
-    :intensity => 2)
+    :name => "Uphill Running (3mph/30 minutes)",
+    :descr => "Running uphill makes the traditional running exercise more difficult and burns more calories for less time.",
+    :calories => 400,
+    :intensity => 4)
     
 Exercise.create(
-    :name => "Indoor Cycling", 
-    :descr => "Indoor cycling is a form of exercise that focuses on endurance, stamina, strength, intensity, intervals and recovery.", 
-    :calories => 200, 
-    :intensity => 1)
-    
+    :name => "Sprinting (2.5 minutes)",
+    :descr => "Sprinting is hight intensity and gets your heart rate going fast, burning calories in a shorter interval of time.",
+    :calories => 200,
+    :intensity => 4)
+
+#Exercises with intensity 5
 Exercise.create(
     :name => "Dragon Flag", 
     :descr => "1.) Start off laying on a decline or flat bench and grabbing the end of it behind your head with both hands.
@@ -136,8 +186,15 @@ Exercise.create(
         
         7.) Repeat for as many reps and sets as desired.", 
     :calories => 320, 
-    :intensity => 3)
+    :intensity => 5)
+    
+Exercise.create(
+    :name => "Jogging (6mph/30 minutes)",
+    :descr => "Same as all the other running stuff",
+    :calories => 330,
+    :intensity => 5)
 
+#Exercises with intensity 6
 Exercise.create(
     :name => "Hand Stand Push Up", 
     :descr => "2.) Slowly kick your feet up against the wall, still keeping your arms straight, and walk your way up the wall with your feet so that your legs and arms are fully extended.
@@ -150,7 +207,13 @@ Exercise.create(
         
         6.) Repeat for as many reps and sets as desired.", 
     :calories => 200, 
-    :intensity => 3)
+    :intensity => 6)
+
+Exercise.create(
+    :name => "Aerobics (intense/30 minutes)",
+    :descr => "Same as the other aerobics put tougher blah blah",
+    :calories => 500,
+    :intensity => 6)
 
 Diet.create(:name => "Apple (Golden Delicious)", :descr => "", :calories => 62)
 Diet.create(:name => "Orange", :descr => "", :calories => 45)
